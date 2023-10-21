1f3b56e824eb0f0f8edcfee110c2b7bc665c2e5e
Falensi Azmi <falensiazmi@gmail.com>
2023-10-21 09:28:16 +0700

    feat:add client-server style like zoom

--- client.py
+++ client.py
@@ -3,33 +3,73 @@ import time
 import cv2 as cv
 import numpy as np
 import threading
+import pickle
 
 HOST = '192.168.0.2'
 PORT = 19122
 BUFFER_SIZE = 1048576
+FRAME_WIDTH = 640
+FRAME_HEIGHT = 480
+CONNECTIONS = dict()
+
+frame_bytes = b''
+def start_camera():
+    cap = cv.VideoCapture(0)
+    cap.set(3, FRAME_WIDTH)
+    cap.set(4, FRAME_HEIGHT)
+
+    while True:
+        global frame_bytes
+        global frame
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture frame.")
+            break
+        frame = cv.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))
+        frame_bytes = cv.imencode('.jpg',frame)[1].tobytes()
+
+    cap.release()
+    cv.destroyAllWindows()
+
+camera_thread = threading.Thread(target=start_camera)
+camera_thread.start()
+
+while frame_bytes == b'':
+    pass
+
+# def send_thread(user_socket, frame_bytes):
+#     user_socket.send(frame_bytes)
 
 def start_receiver():
-    global reconstructed_frame
     while True:
+        global CONNECTIONS
         user_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         user_socket.connect((HOST,PORT))
+
+        # sending_thread = threading.Thread(target=send_thread,
+        #                                   args=(user_socket, frame_bytes))
+        # sending_thread.start()
+        user_socket.send(frame_bytes)
+        print("sending data")
+
         received_data = b''
         while True:
             data_chunk = user_socket.recv(BUFFER_SIZE)
             if not data_chunk:
                 break
             received_data += data_chunk
-        
-        frame_data = np.frombuffer(received_data, dtype=np.uint8)
-        reconstructed_frame = cv.imdecode(frame_data, cv.IMREAD_COLOR)
-        print(reconstructed_frame)
-        # time.sleep(0)
+        print("received data")
+        user_socket.close()
+        CONNECTIONS = pickle.loads(received_data)
+        time.sleep(0.3)
 
 receiver_thread = threading.Thread(target=start_receiver)
 receiver_thread.start()
-time.sleep(3)
 
 while True:
-    cv.imshow('Received Frame', reconstructed_frame)
-    if cv.waitKey(1) == ord('q'):
-        break
+    for host in CONNECTIONS.keys():
+        frame_data = np.frombuffer(CONNECTIONS[host], dtype=np.uint8)
+        reconstructed_frame = cv.imdecode(frame_data, cv.IMREAD_COLOR)
+        cv.imshow(str(host),reconstructed_frame)
+        if cv.waitKey(1) == ord('q'):
+            break
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-import cv2 as cv
+import pickle
 
 HOST = '192.168.0.2'
 PORT = 19122
@@ -8,37 +8,67 @@ BUFFER_SIZE = 1048576
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
 
+CONNECTIONS = dict()
+
+# def send_thread(client_socket, data):
+#     client_socket.sendall(data)
+
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
-    server.listen(5)
+    server.listen(3)
+    print("Server Online!")
 
     while True:
-        client_socket, _ = server.accept()
-        client_socket.sendall(frame_bytes)
+        client_socket, client_address = server.accept()
+        client_address = client_address[0]
+        print("accepting connection from: ", str(client_address))
+
+        if client_address not in CONNECTIONS.keys():
+            CONNECTIONS[client_address] = b''
+        
+        data = pickle.dumps(CONNECTIONS)
+        # sending_thread = threading.Thread(target=send_thread,
+        #                                   args=(client_socket, data))
+        # sending_thread.start()
+        client_socket.sendall(data)
+        print("sending data")
+
+        received_data = b''
+        received_data = client_socket.recv(BUFFER_SIZE)
+        # while True:
+        #     print('a')
+        #     data_chunk = client_socket.recv(BUFFER_SIZE)
+        #     print('b')
+        #     if not data_chunk:
+        #         break
+        #     received_data += data_chunk
+        print("received data")
         client_socket.close()
-        # print(frame)
+
+        CONNECTIONS[client_address] = received_data
         
 server_thread = threading.Thread(target=start_server)
 server_thread.start()
 
-cap = cv.VideoCapture(0)
-cap.set(3, FRAME_WIDTH)
-cap.set(4, FRAME_HEIGHT)
 
-while True:
-    global frame_bytes
-    global frame
-    ret, frame = cap.read()
-    if not ret:
-        print("Error: Failed to capture frame.")
-        break
-    frame = cv.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))
-    frame_bytes = cv.imencode('.jpg',frame)[1].tobytes()
+# cap = cv.VideoCapture(0)
+# cap.set(3, FRAME_WIDTH)
+# cap.set(4, FRAME_HEIGHT)
 
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
+# while True:
+#     global frame_bytes
+#     global frame
+#     ret, frame = cap.read()
+#     if not ret:
+#         print("Error: Failed to capture frame.")
+#         break
+#     frame = cv.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))
+#     frame_bytes = cv.imencode('.jpg',frame)[1].tobytes()
 
-cap.release()
-cv.destroyAllWindows()
+#     cv.imshow('frame', frame)
+#     if cv.waitKey(1) == ord('q'):
+#         break
+
+# cap.release()
+# cv.destroyAllWindows()
